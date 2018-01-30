e038d5f025ebf8bf6f6ada7a0dd6d5a5cfe76dcf
Maximilian Röll <maximilian.roell@plentymarkets.com>
2018-01-30 16:16:19 +0100

    ADDED: comments to the unit test example

--- test/unitTest.js
+++ test/unitTest.js
@@ -1,5 +1,7 @@
-const assert = require('chai').assert;
+const assert = require('chai').assert;                            
+//http://chaijs.com/ is a BDD & TDD language which runs at NodeJS and can be combined with testing frameworks like "mocha" 
 
+//This function uses a "chai" method named "assert.equal" to check if the method "Math.sqrt" returns the expected value  
 function checkSqrtEqualsExpectedValue(square,root)
 {
     it(`result of sqrt(${square}) === ${root} `,function()
@@ -8,12 +10,17 @@ function checkSqrtEqualsExpectedValue(square,root)
     })
 }
 
-describe('sqrt function',function()
+describe('sqrt function Unittest',function()
 {
+    /*I was forced to make an exeption to test the value of -1 because the root of it was NaN(not a number)
+    This function is used to check if the root of -1 does equal NaN */
+
     it(`result of sqrt(-1) === NaN`,function()
     {
         assert.isNaN(Math.sqrt(-1));
     })
+
+    //The function gets called with different parameters to check if the result matches the expected one
     checkSqrtEqualsExpectedValue(0,0);
     checkSqrtEqualsExpectedValue(4,2);
     checkSqrtEqualsExpectedValue(16,4);
